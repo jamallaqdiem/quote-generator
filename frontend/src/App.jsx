4ef624e5a54e9a706ce070ed59b0a41d3332f800
Jamal Laqdiem <jlaqdiem@gmail.com>
2026-05-09 08:14:58 +0100

    Implemeted the UI, using react,vite.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [quote, setQuote] = useState(null);
+  const getQuote = async () => {
+    try {
+      const response = await fetch("http://localhost:3000", {
+        method: "GET",
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error, status ${response.status}`);
+      }
+      const data = await response.json();
+      setQuote(data);
+    } catch (err) {
+      console.error("Error fetching quotes", err);
+    }
+  };
+  useEffect(() => {
+    getQuote();
+  }, []);
+  if (!quote) return <p>Loading Quotes...</p>;
+  return (
+    <div className="quote-card">
+      <h1> Quote Generator </h1>
+      <div className="quote-content">
+        <h3>{quote.quote}</h3>
+        <span>{quote.author}</span>
+      </div>
+      <button onClick={() => getQuote()}>Get Quote</button>
+    </div>
+  );
+}
+
+export default App;
